4fa7713a4784575012b24ce368c945b3bf682436
Oleg Melnikov <olezhka.melnikov.1993@mail.ru>
2023-10-12 01:28:51 +0300

    Add command for implicit wait and is_element_present for catching exceptions

--- conftest.py
+++ conftest.py
@@ -1,8 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 @pytest.fixture(scope="class")
 def browser():
     browser = webdriver.Chrome()
     yield browser
     browser.quit()
+
+def __init__(self, browser, url, timeout=10):
+    self.browser = browser
+    self.url = url
+    self.browser.implicitly_wait(timeout)
+
+def is_element_present(self, how, what):
+    try:
+        self.browser.find_element(how, what)
+    except (NoSuchElementException):
+        return False
+    return True
